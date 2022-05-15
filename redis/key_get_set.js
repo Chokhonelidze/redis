11d18c4613d643749ae1a5a50fee98b7085a8c55
George Chokhonelidze <g_chokhonelidze@outlook.com>
2022-05-14 19:53:45 -0500

    fix

--- redis/key_get_set.js
+++
@@ -1,8 +0,0 @@
-var redis  = require("redis");
-var client = redis.createClient();
-client.connect();
-client.set("my_key","Hello World!");
-client.get("my_key",function(err,reply){
-    console.log(reply);
-});
-client.quit();
